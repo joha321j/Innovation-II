44d1306f7e878d2e22d5859d50b5e5e38d01c4b1
Rick0335 <Rick0335@edu.eal.dk>
2019-01-30 23:23:44 +0100

    Finale

--- Studys/Studys/StudentMainWindow.xaml.cs
+++ Studys/Studys/StudentMainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Studys.View.Categories;
 
 namespace Studys.View
 {
@@ -24,6 +25,82 @@ namespace Studys.View
         {
             InitializeComponent();
         }
+
+
+        // Button Projects - Click
+        private void BtnProjects_Click(object sender, RoutedEventArgs e)
+        {
+            if (CategoriesFrame.Content == string.Empty)
+            {
+                CategoriesFrame.Content = new CategoriesPage(CategoriesFrame, listBox);
+            }
+            else
+            {
+                CategoriesFrame.Content = string.Empty;
+            }
+        }
+
+        // Button FrontPage - Click
+        //--- Show all projects
+        private void BtnFrontPage_Click(object sender, RoutedEventArgs e)
+        {
+            GridMessages.Visibility = Visibility.Hidden;
+            listBoxMyProjects.Visibility = Visibility.Hidden;
+            GridProjectDescription.Visibility = Visibility.Hidden;
+
+            listBox.Visibility = Visibility.Visible;
+        }
+
+        // Button Read more - click
+        //--- Add 'tab' button
+        private void BtnReadMore_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = new Button();
+            button.Name = "BtnProjectTab";
+            button.Background = null;
+            button.Width = 75;
+            button.FontFamily = new FontFamily("Rockwell");
+            button.Content = "Opslag";
+            button.Click += Button_Click;
+
+            //Width="75" FontFamily="Rockwell" Background="White"
+
+            StackTabs.Children.Add(button);
+        }
+
+        // 'tab' button click
+        //--- Hide all projects, show clicked project description
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Visibility = Visibility.Hidden;
+            listBoxMyProjects.Visibility = Visibility.Hidden;
+            GridMessages.Visibility = Visibility.Hidden;
+
+            GridProjectDescription.Visibility = Visibility.Visible;
+        }
+
+
+
+        // BtnMyProjects - Click
+        private void BtnMyProjects_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Visibility = Visibility.Hidden;
+            GridProjectDescription.Visibility = Visibility.Hidden;
+            GridMessages.Visibility = Visibility.Hidden;
+
+            listBoxMyProjects.Visibility = Visibility.Visible;
+        }
+
+        // BtnMessages - click
+        private void BtnMessages_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Visibility = Visibility.Hidden;
+            GridProjectDescription.Visibility = Visibility.Hidden;
+            listBoxMyProjects.Visibility = Visibility.Hidden;
+
+            GridMessages.Visibility = Visibility.Visible;
+        }
+
 
         private void BtnMinimize_Click(object sender, RoutedEventArgs e)
         {
@@ -41,5 +118,8 @@ namespace Studys.View
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
+
+        
+
     }
 }
---
+++ Studys/Studys/View/Categories/CategoriesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Studys.View.Categories
+{
+    /// <summary>
+    /// Interaction logic for CategoriesPage.xaml
+    /// </summary>
+    public partial class CategoriesPage : Page
+    {
+        private Frame _categoriesFrame;
+        private ListBox _listBox;
+        public CategoriesPage(Frame categoriesFrame, ListBox listBox)
+        {
+            InitializeComponent();
+            _categoriesFrame = categoriesFrame;
+            _listBox = listBox;
+        }
+
+        private void BtnDesignCategory_Click(object sender, RoutedEventArgs e)
+        {
+            _categoriesFrame.Content = new DesignCategory();
+            _listBox.Visibility = Visibility.Visible;
+        }
+    }
+}
